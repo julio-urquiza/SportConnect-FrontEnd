6030c20f4c2868bb6487024479d321b6b54eff1c
Lucia Yebara <luciabyebara@gmail.com>
2026-06-11 16:53:07 -0300

    Ajustes en navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <>
-        </>
+        <header className="bg-gray-800 p-4 text-white">
+            <nav className="flex felx-col md:flex-row justify-around">
+                <h1 className="text-xl front-bold mb4 md:mb-0">SportConnect</h1>
+            </nav>
+        <header/>
     )
 }
 
